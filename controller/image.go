b85bb8cd76ced7663db4ae1358594a31638c9732
yosua.tan <yosua.siswanto@lumoshive.com>
2024-03-16 15:39:16 +0700

    update

--- controller/image.go
+++ controller/image.go
@@ -35,38 +35,32 @@ func NewImageController(s3 library.S3) Image {
 func (c Image) ImageUpload(ctx *fiber.Ctx) (int, string, interface{}, error) {
 
 	var url string
-	form, err := ctx.MultipartForm()
+	file, err := ctx.FormFile("file")
+	if err != nil {
+		return http.StatusBadRequest, "upload image fail", nil, err
+	}
+
+	// Check file size
+	if file.Size < minFileSize {
+		return http.StatusBadRequest, "file size must be at least 10 KB", nil, fmt.Errorf("file size is less 10 KB")
+	}
+	if file.Size > maxFileSize {
+		return http.StatusBadRequest, "file size must be no more than 2 MB", nil, fmt.Errorf("file size is more than 2 MB")
+	}
+	ext := filepath.Ext(file.Filename)
+	if !allowedFormats[ext] {
+		return http.StatusBadRequest, "file type must be JPG or JPEG", nil, fmt.Errorf("file type not JPG or JPEG")
+	}
+
+	name := generateUUID()
+
+	uploadedFile, err := file.Open()
 	if err != nil {
 		return http.StatusInternalServerError, "upload image fail", nil, err
 	}
-
-	files := form.File["file"]
-
-	for _, file := range files {
-		// Check file size
-		if file.Size < minFileSize {
-			return http.StatusBadRequest, "file size must be at least 10 KB", nil, fmt.Errorf("file size is less 10 KB")
-		}
-		if file.Size > maxFileSize {
-			return http.StatusBadRequest, "file size must be no more than 2 MB", nil, fmt.Errorf("file size is more than 2 MB")
-		}
-
-		// Check file format
-		ext := filepath.Ext(file.Filename)
-		if !allowedFormats[ext] {
-			return http.StatusBadRequest, "file type must be JPG or JPEG", nil, fmt.Errorf("file type not JPG or JPEG")
-		}
-
-		name := generateUUID()
-
-		uploadedFile, err := file.Open()
-		if err != nil {
-			return http.StatusInternalServerError, "upload image fail", nil, err
-		}
-		url, err = c.S3.UploadFile(uploadedFile, name+ext)
-		if err != nil {
-			return http.StatusInternalServerError, "upload image fail", nil, err
-		}
+	url, err = c.S3.UploadFile(uploadedFile, name+ext)
+	if err != nil {
+		return http.StatusInternalServerError, "upload image fail", nil, err
 	}
 
 	return http.StatusOK, "image uploaded successfully", collections.FileUpload{ImageUrl: url}, err
