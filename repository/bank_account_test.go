c7cdacadd0c3adefeaf15d28efe744ef5c049899
hiiamninna <hiiamninna@github.com>
2024-05-02 10:38:04 +0700

    add unit testing

--- repository/bank_account_test.go
+++ repository/bank_account_test.go
@@ -14,6 +14,11 @@ import (
 
 // references : https://github.com/Fadli2001/go-unit-test-testify/blob/master/repository/customer_db_repository_test.go
 
+// TO DO :
+// 1. why testify? and not golang testing?
+// 2. why you should create suite?
+// 3. mock -> must using mocking, because not connect database
+
 var dummyBankAccounts = []collections.BankAccountInput{
 	{
 		ID:                "1",
@@ -62,6 +67,31 @@ func (suite *BankAccountRepositoryTestSuite) TestBankAccountGetByIDSuccess() {
 
 	assert.Nil(suite.T(), err)
 	assert.NotNil(suite.T(), actual)
+}
+
+func (suite *BankAccountRepositoryTestSuite) TestBankAccountCreateSuccess() {
+	ba := dummyBankAccounts[0]
+	suite.mockSql.ExpectExec(regexp.QuoteMeta(`INSERT INTO bank_accounts (name, account_name, account_number, user_id, created_at, updated_at) VALUES ($1, $2, $3, $4, current_timestamp, current_timestamp);`)).WithArgs("AAA", "Account AAA", "123123", "1").WillReturnResult(sqlmock.NewResult(1, 1))
+	repo := NewBankAccountRepository(suite.mockDb)
+	err := repo.Create(ba)
+	assert.Nil(suite.T(), err)
+}
+
+func (suite *BankAccountRepositoryTestSuite) TestBankAccountListSuccess() {
+	rows := sqlmock.NewRows([]string{"id", "name", "account_name", "account_number"})
+
+	for _, data := range dummyBankAccounts {
+		rows.AddRow(data.ID, data.BankName, data.BankAccountName, data.BankAccountNumber)
+	}
+
+	suite.mockSql.ExpectQuery(regexp.QuoteMeta(`SELECT TEXT(id), name, account_name, account_number FROM bank_accounts WHERE user_id = $1 AND deleted_at is null;`)).WillReturnRows(rows)
+	repo := NewBankAccountRepository(suite.mockDb)
+
+	data, err := repo.List("1")
+
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), 2, len(data))
+	assert.Equal(suite.T(), "AAA", data[0].BankName)
 }
 
 func TestBankAccountRepositoryTestSuite(t *testing.T) {
